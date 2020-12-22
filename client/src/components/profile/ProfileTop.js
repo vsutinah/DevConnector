d8f31662b434f9f5036d993f589670e41c64e9d0
vsutinah <52406232+vsutinah@users.noreply.github.com>
2020-12-22 22:21:53 +1100

    Refactored and fixed ProfileTop string bug

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -28,31 +28,20 @@ const ProfileTop = ({ profile: {
                     </a>
                   )
               }
-              {social && social.twitter && (
-                <a href={social.twitter} target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-twitter fa-2x"></i>
-                </a>
-              )}
-              {social && social.facebook && (
-                <a href={social.facebook} target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-facebook fa-2x"></i>
-                </a>  
-              )}
-              {social & social.linkedin && (
-                <a href={social.linkedin} target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-linkedin fa-2x"></i>
-                </a>
-              )}
-              {social & social.youtube && (
-                <a href={social.youtube} target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-youtube fa-2x"></i>
-                </a>
-              )}
-              {social & social.instagram && (
-                <a href={social.instagram} target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-instagram fa-2x"></i>
-                </a>
-              )}
+              {social &&
+          Object.entries(social)
+            .filter(([key, value]) => value)
+            .map(([key, value]) => (
+              <a
+                key={key}
+                href={value}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className={`fab fa-${key} fa-2x`}></i>
+              </a>
+            ))}
+            
           </div>
         </div>
     )
